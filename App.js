17bc8f17255328d080ba65fe574cc89b83983afb
Brix Aaron Monton <69641153+HumanSockPuppet@users.noreply.github.com>
2024-03-12 17:11:16 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Accordian/> */}
+
+      {/* <RandomColor/> */}
+
+      <StarRating/>
+    </div>
+  );
+}
+
+export default App;
